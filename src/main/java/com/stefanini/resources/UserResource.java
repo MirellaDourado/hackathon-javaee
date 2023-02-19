b4080612efdf2b8841fe9a72e32f3cf550a77a53
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-02-19 18:53:32 -0300

    userResource criado

--- src/main/java/com/stefanini/resources/UserResource.java
+++ src/main/java/com/stefanini/resources/UserResource.java
@@ -1,8 +1,48 @@
 package com.stefanini.resources;
 
-import javax.ws.rs.Path;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import com.stefanini.dto.UserDTO;
+import com.stefanini.service.UserService;
 
 @Path("/user")
 public class UserResource {
-  
+  @Inject
+  UserService userService;
+
+  @POST
+  public Response createUser(UserDTO user) {
+    return Response.status(Response.Status.CREATED).entity(userService.createUser(user)).build();
+  }
+
+  @PUT
+  public Response editUser(UserDTO user) {
+    return Response.status(Response.Status.OK).entity(userService.editUser(user)).build();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public Response removeUser(@PathParam("id") Long id) {
+    userService.removeUser(id);
+    return Response.status(Response.Status.ACCEPTED).build();
+  }
+
+  @GET
+  public Response getAllUsers() {
+    return Response.status(Response.Status.OK).entity(userService.listAllUsers()).build();
+  }
+
+  @GET
+  @Path("/aniversariantes")
+  public Response getBirthdayMonthUsers() {
+    return Response.status(Response.Status.OK).entity(userService.listMonthBirthday()).build();
+  }
+
+  @GET
+  @Path("/email/{email}")
+  public Response getProvedorEmail(@PathParam("email") String email) {
+    return Response.status(Response.Status.OK).entity(userService.listProvedorEmail(email)).build();
+  }
 }
